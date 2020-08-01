d43f1826e7d3a26c8878d914373d02ff8ba4e21a
cristian beltran <cristian.1701422500@ucaldas.edu.co>
2020-07-31 19:37:59 -0500

    interface consultar rutas, navegador y mostrar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import FormularioRutas from './Components/FormularioRutas';
+import Navegacion from './Components/Navegacion';
+import MostrarRutas from './Components/MostarRutas';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Navegacion></Navegacion>
+      <div className="container">
+      
+        <div className="col-md-4 text-center">
+              <FormularioRutas ></FormularioRutas> 
+            
+        </div>
+
+      </div>
+      <MostrarRutas></MostrarRutas>
     </div>
   );
 }
