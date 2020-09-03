1777876b680e3521ad6d6330f7b32b71d740f5a8
Lorenzo <lorenzo.zuluaga.kommit@gmail.com>
2020-09-03 01:53:34 -0500

    Deploy

---
+++ server/build/precache-manifest.cf626176be1d94d95f15023a0d7248cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b89cdc72e677afb18e5ff933beac01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ff282896e1805eb7760",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e8fa0a9943b769e6f4e",
+    "url": "/static/js/2.75dc9811.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.75dc9811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff282896e1805eb7760",
+    "url": "/static/js/main.75ae513e.chunk.js"
+  },
+  {
+    "revision": "983b18906a18cdaac582",
+    "url": "/static/js/runtime-main.930cba4a.js"
+  }
+]);
